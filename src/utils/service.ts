3610156e0a6704d95b7e4b8d390dd807e27569da
ValentinZoia <valentinzoia@gmail.com>
2025-02-04 18:20:52 -0300

    add all image to i will use

---
+++ src/utils/service.ts
@@ -0,0 +1,23 @@
+import coffee1Image from "../../assets/coffee1.png";
+import coffee2Image from "../../assets/coffee3.png";
+
+export const servicesData = [
+    {
+      id: 1,
+      img: coffee1Image,
+      title: `Black Coffee`,
+      content: `lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    },
+    {
+      id: 2,
+      img: coffee2Image,
+      title: `Hot Coffee`,
+      content: `lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    },
+    {
+      id: 3,
+      img: coffee1Image,
+      title: `Cold Coffee`,
+      content: `lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    },
+  ];
